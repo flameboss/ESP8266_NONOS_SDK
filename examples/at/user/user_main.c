0057cac29c367208a3204df1e1673e1401d9c62d
Roger Collins <roger@flameboss.com>
2020-01-23 13:21:57 -0500

    Add a AT+FBVER command to track flame boss customized versions of sdk

--- examples/at/user/user_main.c
+++ examples/at/user/user_main.c
@@ -67,82 +67,42 @@ uint32 user_iram_memory_is_enabled(void)
 }
 #endif
 
-// test :AT+TEST=1,"abc"<,3>
+#define FLAMEBOSS_VERSION "FLAMEBOSS_VER: 3.1.1\r\n"
+
 void ICACHE_FLASH_ATTR
-at_setupCmdTest(uint8_t id, char *pPara)
+at_setupCmdFbVer(uint8_t id, char *pPara)
 {
-    int result = 0, err = 0, flag = 0;
-    uint8 buffer[32] = {0};
-    pPara++; // skip '='
-
-    //get the first parameter
-    // digit
-    flag = at_get_next_int_dec(&pPara, &result, &err);
-
-    // flag must be ture because there are more parameter
-    if (flag == FALSE) {
-        at_response_error();
-        return;
-    }
-
-    if (*pPara++ != ',') { // skip ','
-        at_response_error();
-        return;
-    }
-
-    os_sprintf(buffer, "the first parameter:%d\r\n", result);
-    at_port_print(buffer);
-
-    //get the second parameter
-    // string
-    at_data_str_copy(buffer, &pPara, 10);
-    at_port_print_irom_str("the second parameter:");
-    at_port_print(buffer);
-    at_port_print_irom_str("\r\n");
-
-    if (*pPara == ',') {
-        pPara++; // skip ','
-        result = 0;
-        //there is the third parameter
-        // digit
-        flag = at_get_next_int_dec(&pPara, &result, &err);
-        // we donot care of flag
-        os_sprintf(buffer, "the third parameter:%d\r\n", result);
-        at_port_print(buffer);
-    }
-
-    if (*pPara != '\r') {
-        at_response_error();
-        return;
-    }
-
+    at_port_print(FLAMEBOSS_VERSION);
     at_response_ok();
 }
 
 void ICACHE_FLASH_ATTR
-at_testCmdTest(uint8_t id)
+at_testCmdFbVer(uint8_t id)
 {
-    at_port_print_irom_str("at_testCmdTest\r\n");
+    at_port_print(FLAMEBOSS_VERSION);
     at_response_ok();
 }
 
 void ICACHE_FLASH_ATTR
-at_queryCmdTest(uint8_t id)
+at_queryCmdFbVer(uint8_t id)
 {
-    at_port_print_irom_str("at_queryCmdTest\r\n");
+    at_port_print(FLAMEBOSS_VERSION);
     at_response_ok();
 }
 
 void ICACHE_FLASH_ATTR
-at_exeCmdTest(uint8_t id)
+at_exeCmdFbVer(uint8_t id)
 {
-    at_port_print_irom_str("at_exeCmdTest\r\n");
+    at_port_print(FLAMEBOSS_VERSION);
     at_response_ok();
 }
 
+#ifdef AT_UPGRADE_SUPPORT
 extern void at_exeCmdCiupdate(uint8_t id);
+#endif
+
 at_funcationType at_custom_cmd[] = {
-    {"+TEST", 5, at_testCmdTest, at_queryCmdTest, at_setupCmdTest, at_exeCmdTest},
+    {"+FBVER", 6, at_testCmdFbVer, at_queryCmdFbVer, at_setupCmdFbVer, at_exeCmdFbVer},
 #ifdef AT_UPGRADE_SUPPORT
     {"+CIUPDATE", 9,       NULL,            NULL,            NULL, at_exeCmdCiupdate}
 #endif

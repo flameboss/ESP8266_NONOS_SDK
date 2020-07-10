fc14837fac9e924124371a97048a1ca3f72536e5
Roger Collins <roger@flameboss.com>
2020-07-10 16:25:23 -0400

    decouple sdk from app

--- driver_lib/driver/uart.c
+++ driver_lib/driver/uart.c
@@ -30,7 +30,6 @@
 #include "user_interface.h"
 #include "driver/uart.h"
 #include "driver/uart_register.h"
-#include <cli.h>
 
 // UartDev is defined and initialized in rom code.
 extern UartDevice    UartDev;
@@ -296,6 +295,13 @@ uart_test_rx()
 }
 #endif
 
+static void (*user_rx)(uint8_t);
+
+void uart_set_rx_callback(void (*fp)(uint8_t))
+{
+	user_rx = fp;
+}
+
 LOCAL void ICACHE_FLASH_ATTR ///////
 uart_recvTask(os_event_t *events)
 {
@@ -310,7 +316,9 @@ uart_recvTask(os_event_t *events)
         for (idx = 0; idx < fifo_len; idx++) {
             d_tmp = READ_PERI_REG(UART_FIFO(UART0)) & 0xFF;
             uart_tx_one_char(UART0, d_tmp);
-            cli_recv(d_tmp);
+            if (user_rx) {
+                (*user_rx)(d_tmp);
+            }
         }
 
         WRITE_PERI_REG(UART_INT_CLR(UART0), UART_RXFIFO_FULL_INT_CLR | UART_RXFIFO_TOUT_INT_CLR);
